a007b0b4f4aa94abb5162cba66c7e9036343319e
NAGARRO\ratnamadhav <ratnamadhav777@gmail.com>
2022-12-21 16:41:41 +0530

    new code added

---
+++ class3.java
@@ -0,0 +1,11 @@
+package pack;
+
+public class class3 {
+	public static void main(String[] args) {
+		System.out.println("Ratna Madhav");
+	}
+	public void abcd() {
+		System.out.println("Hii");
+	}
+
+}
